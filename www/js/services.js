9f5c4c696221d49db24b148e4f821bb226c84a2c
Juan Roa <jdroa92@gmail.com>
2016-06-20 19:56:13 -0500

    Working at individual view for courses

--- www/js/services.js
+++ www/js/services.js
@@ -1,47 +1,29 @@
-angular.module('starter.services', [])
+angular.module('mooc.services', [])
 
-.factory('Chats', function() {
+.factory('Courses', function() {
   // Might use a resource here that returns a JSON array
 
   // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
-  }];
+  $scope.courses = [];
+    $http.get('http://informaticaeducativaucc.com/api/cursos')
+      .then(function(successResponse){
+        $scope.courses = successResponse.data;
+        console.log(successResponse.data);
+      }, function(errorResponse){
+        $scope.error = errorResponse;
+    });
 
   return {
     all: function() {
-      return chats;
+      return courses;
     },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
+    remove: function(course) {
+      courses.splice(courses.indexOf(course), 1);
     },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
+    get: function(courseId) {
+      for (var i = 0; i < courses.length; i++) {
+        if (courses[i].id === parseInt(courseId)) {
+          return courses[i];
         }
       }
       return null;
