8fa8539537877a5e7cdbf8811eb741e20a8e28b2
Juan Roa <jdroa92@gmail.com>
2016-06-21 16:54:45 -0500

    Course in course detail view working

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,27 +2,6 @@ angular.module('mooc.controllers', [])
 
 .controller('AppCtrl', function() {})
 
-// .controller('CoursesCtrl', function($scope, Courses) {
-
-// 	$scope.courses = Courses.all();
-// 	// $scope.remove = function(course) {
-// 	// 	Courses.remove(course);
-// 	// };
-
-//   // $scope.courses = [];
-//   //   $http.get('http://informaticaeducativaucc.com/api/cursos')
-//   //     .then(function(successResponse){
-//   //       $scope.courses = successResponse.data;
-//   //       console.log(successResponse.data);
-//   //     }, function(errorResponse){
-//   //       $scope.error = errorResponse;
-//   //   });
-// })
-
-// .controller('CourseCtrl', function($scope, $routeParams, Courses) {
-//   $scope.course = Courses.get($stateParams.courseId);
-// });
-
 .controller('CoursesCtrl', function($scope, $http) {
   $scope.courses = [];
   $http.get('http://informaticaeducativaucc.com/api/cursos').then(function(successResponse) {
@@ -33,7 +12,6 @@ angular.module('mooc.controllers', [])
    });
 })
 
-
 .controller('CourseDetailCtrl', function($scope, $stateParams, $http) {
 
   $scope.courseId = $stateParams.courseId;
@@ -41,31 +19,8 @@ angular.module('mooc.controllers', [])
   $scope.course = [];
   $http.get('http://informaticaeducativaucc.com/api/curso/'+$scope.courseId).then(function(successResponse) {
     console.log(successResponse.data);
-    $scope.course = successResponse.data;
+    $scope.course = successResponse.data[0];
   }, function(errorResponse){
      $scope.error = errorResponse;
    });
 });
-
-//   $scope.courses = [
-//     { title: 'Algoritmia I', id: 1 },
-//     { title: 'Programación Orientada a Objetos', id: 2 },
-//     { title: 'Programación Funcional', id: 3 },
-//     { title: 'Desarrollo móvil iOS', id: 4 },
-//     { title: 'Desarrollo móvil Android', id: 5 },
-//     { title: 'Desarrollo web con AngularJS', id: 6 }
-//   ];
-// })
-
-// .controller('CourseCtrl', function($scope, $stateParams) {
-
-// 	$scope.courses = [];
-// 	    $http.get('http://informaticaeducativaucc.com/api/cursos')
-// 	      .then(function(successResponse){
-// 	        $scope.courses = successResponse.data;
-// 	        console.log(successResponse.data);
-// 	    }, function(errorResponse){
-// 	        $scope.error = errorResponse;
-//     });
-
-// });
