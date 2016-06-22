dc23bc200f7bcfc92cd399154afe4f6952a20ac3
Juan Roa <jdroa92@gmail.com>
2016-06-22 09:58:49 -0500

    Fixing header and content issue at course detail

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -12,43 +12,43 @@ angular.module('mooc.controllers', [])
 //    });
 // })
 
-.controller('CoursesCtrl', function($scope, $http) {
+.controller('CoursesCtrl', function($scope, CoursesService) {
 
-  // function refreshCourses() {
-  //   CoursesService.list().then(function(successResponse) {
-  //     $scope.courses = successResponse;
-  //     console.log($scope.courses);
-  //   });
-  // }
-  // refreshCourses();
+  function refreshCourses() {
+    CoursesService.list().then(function(successResponse) {
+      $scope.courses = successResponse;
+      console.log($scope.courses);
+    });
+  }
+  refreshCourses();
 
-  $scope.courses = [];
-  $http.get('http://informaticaeducativaucc.com/api/cursos').then(function(successResponse) {
-    console.log(successResponse.data);
-    $scope.courses = successResponse.data;
-  }, function(errorResponse){
-     $scope.error = errorResponse;
-   });
+  // $scope.courses = [];
+  // $http.get('http://informaticaeducativaucc.com/api/cursos').then(function(successResponse) {
+  //   console.log(successResponse.data);
+  //   $scope.courses = successResponse.data;
+  // }, function(errorResponse){
+  //    $scope.error = errorResponse;
+  //  });
 })
 
-// .controller('CourseDetailCtrl', function($scope, $stateParams, CoursesService) {
-//   console.log($stateParams.courseId);
-//   CoursesService.get($stateParams.courseId).then(function(successResponse) {
-//     $scope.course = successResponse;
-//     console.log(successResponse);
-//   });
-//
-// });
+.controller('CourseDetailCtrl', function($scope, $stateParams, CoursesService) {
+  console.log($stateParams.courseId);
+  CoursesService.get($stateParams.courseId).then(function(successResponse) {
+    $scope.course = successResponse;
+    console.log(successResponse);
+  });
 
-.controller('CourseDetailCtrl', function($scope, $stateParams, $http) {
-  $scope.courseId = $stateParams.courseId;
-  console.log($scope.courseId);
-  $scope.course = [];
-  $http.get('http://informaticaeducativaucc.com/api/curso/'+$scope.courseId)
-  .then(function(successResponse) {
-    //console.log(successResponse.data);
-    $scope.course = successResponse.data[0];
-  }, function(errorResponse){
-     $scope.error = errorResponse;
-   });
 });
+
+// .controller('CourseDetailCtrl', function($scope, $stateParams, $http) {
+//   $scope.courseId = $stateParams.courseId;
+//   console.log($scope.courseId);
+//   $scope.course = [];
+//   $http.get('http://informaticaeducativaucc.com/api/curso/'+$scope.courseId)
+//   .then(function(successResponse) {
+//     //console.log(successResponse.data);
+//     $scope.course = successResponse.data[0];
+//   }, function(errorResponse){
+//      $scope.error = errorResponse;
+//    });
+// });
